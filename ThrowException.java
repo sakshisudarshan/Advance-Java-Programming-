af54086adce6bc9119e23d316fc8fa166a4311c4
sakshisudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2024-07-13 11:44:58 +0530

    Create ThrowException.java

---
+++ ThrowException.java
@@ -0,0 +1,31 @@
+public class Main
+{
+  public static void main (String[]args)
+  {
+	int i = 20;
+	int j = 0;
+
+	  try
+	{
+	  j = 18 / i;
+	  if (j == 0)
+		throw new ArithmeticException ();
+
+	}
+
+	catch (ArithmeticException e)
+	{
+	  j = 18 / 1;
+	  System.out.println("this is the default output " +e);
+	}
+
+
+	catch (Exception e)
+	{
+	  System.out.println ("Something went wrong");
+	}
+
+	System.out.println (j);
+
+  }
+}
