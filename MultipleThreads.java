fcd16cd4a0afaf6174a88b0cc8342bf850539095
sakshisudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2024-07-13 19:02:17 +0530

    Create MultipleThreads.java

---
+++ MultipleThreads.java
@@ -0,0 +1,32 @@
+class A extends Thread
+{
+  public void run () 
+  {
+	for(int i=0;i<=100;i++){
+	    System.out.println("hi");
+	}
+
+  }
+}
+
+class B extends Thread
+{
+  public void run () 
+  {
+	for(int i=0;i<=100;i++){
+	    System.out.println("hey");
+	}
+
+  }
+}
+public class Main
+{
+  public static void main (String[]args)
+  {
+    A obj1=new A();
+    B obj2=new B();
+    
+    obj1.start();
+    obj2.start();
+  }
+}
